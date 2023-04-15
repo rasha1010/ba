72543e6091f0a0814482283e76587b63663cd2c8
rasha1010 <110055139+rasha1010@users.noreply.github.com>
2023-04-15 23:44:05 +0300

    Add files via upload

---
+++ file/routes.js
@@ -0,0 +1,55 @@
+routes = [
+  {
+    path: "/root/",
+    url: "./index.html",
+  },
+  /*APPs*/
+  {
+    path: "/jailbreak/",
+    url: "./log/jailbreak.html",
+  },
+  {
+    path: "/uncover/",
+    url: "./info/uncover.html",
+  },
+  {
+    path: "/chimera/",
+    url: "./info/chimera.html",
+  },
+  /*APPs*/
+  {
+    path: "/apps/",
+    url: "./log/apps.html",
+  },
+  {
+    path: "/videostar/",
+    url: "./info/videostar.html",
+  },
+    /*MODIFIED*/
+  {
+    path: "/modified/",
+    url: "./log/modified.html",
+  },
+  {
+    path: "/youtube/",
+    url: "./info/youtube.html",
+  },
+  /*GAMES*/
+  {
+    path: "/games/",
+    url: "./log/games.html",
+  },
+  {
+    path: "/subway/",
+    url: "./info/subway.html",
+  },
+   /*STORES*/
+  {
+    path: "/store/",
+    url: "./log/store.html",
+  },
+  {
+    path: "/esign/",
+    url: "./info/esign.html",
+  },
+];
